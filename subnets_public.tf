6c9b6f3e79a78bf829bb6a1eabd82816feea66be
leolm83 <leonardo.mateus01@hotmai.com>
2025-04-27 11:23:26 -0300

    feat: adiciona tfvars[A

--- subnets_public.tf
+++ subnets_public.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "leolms_terraform_public_subnet_1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
@@ -9,7 +9,7 @@ resource "aws_subnet" "leolms_terraform_public_subnet_1a" {
 }
 resource "aws_subnet" "leolms_terraform_public_subnet_1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
   tags = {
