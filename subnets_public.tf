98948cad6d8dd343163a8d36d1ff635fb446b227
leolm83 <leonardo.mateus01@hotmai.com>
2025-04-27 11:28:53 -0300

    feat: variavel de nome da regiao

--- subnets_public.tf
+++ subnets_public.tf
@@ -1,18 +1,20 @@
 resource "aws_subnet" "leolms_terraform_public_subnet_1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "us-east-1a"
+  availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
   tags = {
-    Name = "leolms-terraform-public-subnet-1a"
+    Name                     = "leolms-terraform-public-subnet-1a"
+    "kubernetes.io/role/elb" = 1
   }
 }
 resource "aws_subnet" "leolms_terraform_public_subnet_1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "us-east-1b"
+  availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
   tags = {
-    Name = "leolms-terraform-public-subnet-1b"
+    Name                     = "leolms-terraform-public-subnet-1b"
+    "kubernetes.io/role/elb" = 1
   }
 }
