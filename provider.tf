05afc06b6a9759c815d60d737dfb1084bcf9392b
leolm83 <leonardo.mateus01@hotmai.com>
2025-04-27 01:05:44 -0300

    feat: fix lint

--- provider.tf
+++ provider.tf
@@ -1,9 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.96.0"
     }
+  }
+  #   envia config para o bucket no s3
+  backend "s3" {
+    bucket = "leolms-tfstate-bucket"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
