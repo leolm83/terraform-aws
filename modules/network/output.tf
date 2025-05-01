112094db8dae87669a66019a97239b4b47b9501d
leolm83 <leonardo.mateus01@hotmai.com>
2025-05-01 11:47:01 -0300

    feat: output subnets

---
+++ modules/network/output.tf
@@ -0,0 +1,12 @@
+output "public_subnet_1a" {
+  value = aws_subnet.leolms_terraform_public_subnet_1a.id
+}
+output "public_subnet_1b" {
+  value = aws_subnet.leolms_terraform_public_subnet_1b.id
+}
+output "private_subnet_1a" {
+  value = aws_subnet.leolms_terraform_private_subnet_1a.id
+}
+output "private_subnet_1b" {
+  value = aws_subnet.leolms_terraform_private_subnet_1b.id
+}
---
+++ output.tf
@@ -0,0 +1,12 @@
+output "public_subnet_1a" {
+  value = module.leolm_terraform_network.public_subnet_1a
+}
+output "public_subnet_1b" {
+  value = module.leolm_terraform_network.public_subnet_1b
+}
+output "private_subnet_1a" {
+  value = module.leolm_terraform_network.private_subnet_1a
+}
+output "private_subnet_1b" {
+  value = module.leolm_terraform_network.private_subnet_1b
+}
