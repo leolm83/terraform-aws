0a7c81ed7d2869ae209a501e22c72e41cd3901e9
leolm83 <leonardo.mateus01@hotmai.com>
2025-05-01 11:07:17 -0300

    feat: adicionando rt subnets privadas, associacoes e inicio do modulo de network

---
+++ modules/network/private_subnet_route_table_1a.tf
@@ -0,0 +1,13 @@
+resource "aws_route_table" "leolms_terraform_private_subnet_route_table_1a" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.leolms_terraform_nat_gateway_1a.id
+  }
+  tags = merge(local.tags,
+    {
+      Name = "${var.project_name}-private-route-table-1a"
+    }
+  )
+}
